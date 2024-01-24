213631375db567a76e8158fac571f309ad269d99
Adrian Melendez <Adrian.Melendez@caemsi.com>
2024-01-24 16:29:40 -0500

    link in pop up

---
+++ scripts/background.js
@@ -0,0 +1,7 @@
+// // background.js
+
+// chrome.runtime.onStartup.addListener(function (details) {
+//     if (details.reason === 'install') {
+//         chrome.tabs.create({ url: "https://www.reddit.com/r/antiwork/" });
+//     }
+// });
---
+++ scripts/popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var openTabLink = document.getElementById('openTabLink');
+
+    openTabLink.addEventListener('click', function () {
+        chrome.tabs.create({ url: "https://old.reddit.com/r/awfuleverything/" });
+    });
+});
