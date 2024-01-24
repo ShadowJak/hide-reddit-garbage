abafd813b4b2bc9bd96b0393aac8ba6e4f457c1e
Adrian Melendez <Adrian.Melendez@caemsi.com>
2024-01-23 20:57:29 -0500

    messy but getting places

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,31 @@
 const article = document.querySelector("article");
 
+window.addEventListener('load', function() {
+    // alert('ok');
+    // const testButton = document.getElementById("dropdown-list-0-toggle");
+    // const testButton = document.querySelector("button");
+    // const stuff = this.document.getElementsByTagName("button");
+    // const moreStuff = this.document.getElementsByClassName("devsite-thumb devsite-thumb-up");
+    // const moreStuff = this.document.getElementsByTagName("button");
+    // console.log('testestestsetsetsetsets');
+    // console.log(11, moreStuff);
+    // for (i = 0; i < moreStuff.length; i++) {
+    //     console.log(i, moreStuff[i].ariaLabel);
+    // }
+    // console.log(22, moreStuff[0].ariaLabel);
+
+    // moreStuff[7].click();
+
+    const anchors  = document.getElementsByTagName("a");
+    console.log(11, anchors)
+    for (i = 0; i < anchors.length; i++) {
+        // console.log(i, anchors[i].innerText);
+        if (anchors[i].innerText === 'hide') {
+            anchors[i].click();
+        }
+    }
+})
+
 // `document.querySelector` may return null if the selector doesn't match anything.
 if (article) {
   const text = article.textContent;
